650746124fbb008746cb9d58e2136d3c584850a7
Krishna <krishnakeec@gmail.com>
2023-10-10 00:31:55 +0300

    check bg when resizing

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import pic1 from '../assets/team-1.jpg'
 function Main() {
     return (
        <div className='bg-blue-300 w-full'>
-        <img src={pic1} alt='Trophy celebrating' className='w-screen'/>
+        {/* <img src={pic1} alt='Trophy celebrating' className='w-screen'/> */}
        </div> 
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,32 @@
-import React, {useState} from 'react'
+
 import logo from '../assets/logo.png'
 
 function Navbar() {
    
-    const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
 
     return (
-        <div className={`${menuOpen ? 'open' : ''} flex flex-row flex-wrap justify-center items-center min-h-fit w-screen bg-transparent fixed top-0`}>
+        <div className={` flex flex-row flex-nowrap justify-between items-center min-h-fit w-screen bg-slate-200 fixed `}>
 
-            <div onClick={toggleMenu}>
-                <i className={`fa ${menuOpen ? 'fa-times' : 'fa-bars'} flex-none w-40 shrink-0 text-center text-3xl text-white
-                    bg-gradient-to-r via-red-700 from-blue-800  rounded-t-full rounded-b-full cursor-pointer`}></i>
-                    {menuOpen && (
-                            <ul className="table-of-contents list-none p-5 absolute top-64 left-0 bg-gradient-to-r from-red-500 to-blue-500 transform translate-x-2">
-                                <li className="p-10 pr-30 text-white text-16 cursor-pointer hover:bg-opacity-20 hover:bg-white hover:bg-opacity-20">About</li>
-                                <li>Events</li>
-                                <li>Calendar</li>
-                                <li>Contacts</li>
-                            </ul>
-                    )}        
-            </div>
-
-            <div className="logo flex-1 w-auto  shrink-0 basis-1/5 flex justify-center">
+            <div className="logo  w-1/6  flex  justify-center">
                 <img src={logo} alt="Logo" className="h-36 object-contain rounded-full" />
             </div>
 
-            <button className="h-9 w-40 p-0.5 text-md text-white bg-gradient-to-l via-red-700 from-blue-800 rounded-t-full rounded-b-full">
-                SignUp/ LogIn
-            </button>
+            <div className='w-3/6 mr-5' >
+               
+                            <ul className=" nav flex flex-row justify-around w-full  text-white bg-gradient-to-r from-red-600 to-blue-900 rounded-t-full rounded-b-full">
+                                <li>About</li>
+                                <li>Events</li>
+                                <li>Calendar</li>
+                                <li>Gallery</li>
+                                <li>SignUp/LogIn</li>
+                            </ul>
+                         
+            </div>
+
+            
+
+            
 
         </div>
     );
